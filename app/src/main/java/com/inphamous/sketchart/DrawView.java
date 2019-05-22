a759ea7a97e9796599b993877d56c523ef00d8d7
Cindy Pham <cindy619mysterio@yahoo.com>
2019-05-21 21:22:07 -0500

    Set up drawing layout

---
+++ app/src/main/java/com/inphamous/sketchart/DrawView.java
@@ -0,0 +1,18 @@
+package com.inphamous.sketchart;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class DrawView extends View {
+
+    // Default constructor
+    public DrawView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        setUpDraw();
+    }
+
+    private void setUpDraw() {
+        // Set up the drawing area
+    }
+}
--- app/src/main/java/com/inphamous/sketchart/PlayActivity.java
+++ app/src/main/java/com/inphamous/sketchart/PlayActivity.java
@@ -1,7 +1,9 @@
 package com.inphamous.sketchart;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+    }
+
+    public void paintClicked(View view) {
     }
 }
