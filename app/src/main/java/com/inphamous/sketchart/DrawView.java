69a3906252fede627162534a207b1406e596824a
Cindy Pham <cindy619mysterio@yahoo.com>
2019-05-22 19:32:12 -0500

    Implement painting

--- app/src/main/java/com/inphamous/sketchart/DrawView.java
+++ app/src/main/java/com/inphamous/sketchart/DrawView.java
@@ -1,10 +1,27 @@
 package com.inphamous.sketchart;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.view.View;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 public class DrawView extends View {
+    private Path drawPath;      // Drawing path
+    private Paint drawPaint;    // Drawings and Canvas paint
+    private Paint canvasPaint;
+    private int paintColor = 0x000000;   // Initial color
+    private int backgroundColor = 0xFFFFFF; // Background canvas color
+    private Canvas drawCanvas;           // Canvas
+    private Bitmap canvasBitmap;         // Canvas Bitmap
+    private boolean erase = false;       // Flag for erasing
+    private int strokeSize = 20;         // Stroke size
 
     // Default constructor
     public DrawView(Context context, AttributeSet attributeSet) {
@@ -14,5 +31,73 @@ public class DrawView extends View {
 
     private void setUpDraw() {
         // Set up the drawing area
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        // Set the initial color
+        drawPaint.setColor(paintColor);
+
+        // Set initial path
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(strokeSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        // Instantiate canvas Paint
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+        drawCanvas.drawColor(backgroundColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Get position of user's touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        // Draw the path
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    public void setColor(String newColor) {
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
+    public void setErase(boolean isErase) {
+        erase = isErase;
+        if (erase) {
+            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        }
     }
 }
--- app/src/main/java/com/inphamous/sketchart/PlayActivity.java
+++ app/src/main/java/com/inphamous/sketchart/PlayActivity.java
@@ -1,18 +1,77 @@
 package com.inphamous.sketchart;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.graphics.Paint;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
+import java.util.UUID;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends Activity implements OnClickListener {
+    private DrawView drawView;
+    // UI
+    private ImageButton currPaint;
+    private ImageButton drawButton;
+    private ImageButton eraseButton;
+    // Brush sizes
+    private float smallBrush;
+    private float mediumBrush;
+    private float largeBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        drawView = findViewById(R.id.draw);
+        LinearLayout paintLayout = findViewById(R.id.paint_colors);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+
+        drawButton = findViewById(R.id.draw_button);
+        drawButton.setOnClickListener(this);
+
+        eraseButton = findViewById(R.id.erase_button);
+        eraseButton.setOnClickListener(this);
     }
 
     public void paintClicked(View view) {
+        drawView.setErase(false);
+        // Update color
+        if (view != currPaint) {
+            ImageButton imageView = (ImageButton) view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint = (ImageButton) view;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Draw selected
+        if (view.getId() == R.id.draw_button) {
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
+            drawView.setErase(false);
+        }
+        // Erase selected
+        else if (view.getId() == R.id.erase_button) {
+            drawView.setErase(true);
+        }
     }
 }
