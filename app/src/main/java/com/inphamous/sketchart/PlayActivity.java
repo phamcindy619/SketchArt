e80327ef4c81d44ae3a94be7414973cb287aae33
Cindy Pham <cindy619mysterio@yahoo.com>
2019-05-24 00:52:33 -0500

    Implement save (contains bugs regarding save location - root folder)

--- app/src/main/java/com/inphamous/sketchart/PlayActivity.java
+++ app/src/main/java/com/inphamous/sketchart/PlayActivity.java
@@ -2,14 +2,24 @@ package com.inphamous.sketchart;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.media.Image;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.view.View.OnClickListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 import android.provider.MediaStore;
 import android.app.AlertDialog;
@@ -23,11 +33,15 @@ public class PlayActivity extends Activity implements OnClickListener {
     private ImageButton drawButton;
     private ImageButton eraseButton;
     private ImageButton newButton;
+    private ImageButton saveButton;
 
     // Brush sizes
     private float smallBrush;
     private float mediumBrush;
     private float largeBrush;
+
+    // Debug
+    private static final String LOG_TAG = PlayActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +67,9 @@ public class PlayActivity extends Activity implements OnClickListener {
 
         newButton = findViewById(R.id.new_button);
         newButton.setOnClickListener(this);
+
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(this);
     }
 
     public void paintClicked(View view) {
@@ -78,7 +95,6 @@ public class PlayActivity extends Activity implements OnClickListener {
             brushDialog.setTitle("Brush size:");
             brushDialog.setContentView(R.layout.brush_selector);
 
-            brushDialog.show();
             // Small brush size has been clicked
             ImageButton smallButton = brushDialog.findViewById(R.id.small_brush);
             smallButton.setOnClickListener(new OnClickListener() {
@@ -114,6 +130,7 @@ public class PlayActivity extends Activity implements OnClickListener {
                     brushDialog.dismiss();
                 }
             });
+            brushDialog.show();
         }
         // Erase selected
         else if (view.getId() == R.id.erase_button) {
@@ -121,7 +138,6 @@ public class PlayActivity extends Activity implements OnClickListener {
             eraserDialog.setTitle("Eraser size:");
             eraserDialog.setContentView(R.layout.brush_selector);
 
-            eraserDialog.show();
             // Small eraser size has been clicked
             ImageButton smallButton = eraserDialog.findViewById(R.id.small_brush);
             smallButton.setOnClickListener(new OnClickListener() {
@@ -154,6 +170,7 @@ public class PlayActivity extends Activity implements OnClickListener {
                     eraserDialog.dismiss();
                 }
             });
+            eraserDialog.show();
         }
         // New selected
         else if (view.getId() == R.id.new_button) {
@@ -175,6 +192,49 @@ public class PlayActivity extends Activity implements OnClickListener {
                 }
             });
             newDialog.show();
+        }
+        // Save selected
+        else if (view.getId() == R.id.save_button) {
+            // Confirm that user wants to save their drawing
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Convert view into image
+                    Bitmap bitmap = Bitmap.createBitmap(drawView.getWidth(), drawView.getHeight(), Bitmap.Config.ARGB_8888);
+                    drawView.draw(new Canvas(bitmap));
+
+                    File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                    File file = new File(path, UUID.randomUUID().toString() + ".jpg");
+                    Log.d(LOG_TAG, "The path is: " + path);
+
+                    // Save the image in SD card
+                    try {
+                        path.mkdirs();
+                        Log.d(LOG_TAG, "Pictures directory found.");
+                        OutputStream os = new FileOutputStream(file);
+                        Log.d(LOG_TAG, "Created output stream.");
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+                        Log.d(LOG_TAG, "Bitmap is saved.");
+                        os.close();
+                        Toast savedToast = Toast.makeText(getApplicationContext(), "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    catch (Exception e) {
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(), "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
         }
     }
 }
